fb16d1a4ae055d012b25f8c9d958d9c9c3d2b4d2
Thomas Schank <DrTom@schank.ch>
2023-10-31 19:55:06 +0100

    WIP include uasyncio; does compile but not accessible in firmware

--- bricks/_common/manifest.py
+++ bricks/_common/manifest.py
@@ -1,9 +1,15 @@
 import pathlib
 
+# print(f">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> MPYDIR: {MPYDIR}")
+# include("$(MPY_DIR)/extmod/uasyncio")
+
+include("./modules/uasyncio")
+
 modules = list(pathlib.Path("./modules").glob("*.py"))
 
 if any(modules):
     for m in modules:
+        print(f"INCLUDING {m} as a module.")
         path, file = m.parts
         print(f"Including {m.stem} as a module.")
         freeze_as_mpy(path, file)
